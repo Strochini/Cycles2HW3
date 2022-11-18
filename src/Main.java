921ccc861d0e1a16ff95411997ff13543069c1f8
Strochini <0.618@bk.ru>
2022-11-18 04:56:01 +0300

    HW3 Done

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,26 @@ public class Main {
     public static void main(String[] args) {
 
         // Task 1
+
+        int startYear = 2022 - 200;
+        int endYear = 2022 + 100;
+        int result = 0;
+
+        for (; result < endYear; result = result + 79) {
+            if (result % 79 == 0 && result > startYear) {
+
+                System.out.println(result);
+            }
+        }
+        // Task 2
+
+        int two = 2;
+        int factor = 1;
+        int result0 = 0;
+
+        for (; factor <= 10; factor ++) {
+            result0 = two * factor;
+            System.out.println(two + "*" + factor + "=" + result0);
+        }
     }
 }
